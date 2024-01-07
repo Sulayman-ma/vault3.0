2f4f41fa2a50180622d87538a0049133187abbba
Sulayman-ma <suleyman.abba@gmail.com>
2024-01-07 01:20:02 +0100

    removed non existing mini cards iimport and render function

--- src/lib/render-cards.js
+++ src/lib/render-cards.js
@@ -1,48 +1,9 @@
-import {
-  SecretMini,
-  WillMini,
-  LegalDocumentMini,
-  MessageMini,
-} from "@/components/assets/mini-cards"
 import { 
   BlankCard,  
   NewAssetCard, 
 } from '@/components/assets/detailed-cards'
 import OtherCard from "@/components/assets/other-card";
 import SecretCard from "@/components/assets/secret-card";
-
-export const renderMiniCard = (group, assetData, setActiveAsset) => {
-  switch (group) {
-    case 'Will':
-      return (
-        <WillMini 
-          setAsActive={setActiveAsset} 
-          assetData={assetData}
-        />
-      );
-    case 'Secret':
-      return (
-        <SecretMini 
-          setAsActive={setActiveAsset} 
-          assetData={assetData}
-        />
-      );
-    case 'Legal Document':
-      return (
-        <LegalDocumentMini 
-          setAsActive={setActiveAsset} 
-          assetData={assetData}
-        />
-      );
-    case 'Special Message':
-      return (
-        <MessageMini 
-          setAsActive={setActiveAsset} 
-          assetData={assetData}
-        />
-      );
-  }
-}
 
 export const renderDetailedCard = (stuff) => {
   switch (stuff.group) {
